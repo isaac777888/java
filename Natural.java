e6f843c378cef0b01372e116fc70816a6b288701
isaac777888 <116713978+isaac777888@users.noreply.github.com>
2022-12-15 16:26:52 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,11 @@
+# java
+import java.util.Scanner;
+public class Natural{
+public static void main(String args[]){
+Scanner input=new Scanner(System.in);
+System.out.println("Enter the number:");
+int number=input.nextInt();
+for(int i=1;i<=number;i++){
+System.out.print(" "+i);
+}
+}
